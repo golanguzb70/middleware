1d3a0f5d0a3b06674b03c9122fb8f9c21ce4ace2
Azizbek <azizbekhojimurodov4@gmail.com>
2023-06-04 07:28:15 +0500

    add gin/basicauth

---
+++ gin/basicauth/main.go
@@ -0,0 +1,34 @@
+package basicauth
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// This is configuration struct of Basic Auth
+type Config struct {
+	// Users is list of users that have access.
+	// There may be a user1 with password1 and user2 with password2 and etc.
+	Users []User `json:"users"`
+	// Restricted Method means that the middleware only applies for method are given.
+	// For example, PUT, POST, PATCH, DELETE methods are given to this field. Middleware check password for request with these REST Methods.
+	RestrictedMethods []string `json:"restricted_methods"`
+	// Restricted urls are the urls that are authoriztion is required.
+	// For example, /v1/user, /v1/user/{key}, /v1/admin
+	// if /v1/user is given, request url is checked for equality.
+	// if /v1/user/{key} is given, request url is check for tthe urls starting with /v1/user  and one other key.
+	// if /v1/user/*  is given, request url is check for all the urls that starts with '/v1/user'.
+	RestrictedUrls []string `json:"restricted_urls"`
+}
+
+type User struct {
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+}
+
+type Auth interface {
+	Middleware(c *gin.Context)
+}
+
+func New(conf *Config) Auth {
+	return conf
+}
---
+++ gin/basicauth/middleware.go
@@ -0,0 +1,72 @@
+package basicauth
+
+import (
+	"encoding/base64"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+// method for checking authorization
+func (cfg *Config) Middleware(ctx *gin.Context) {
+	var (
+		authRequired = false
+		url          = ctx.Request.URL.Path
+		method       = ctx.Request.Method
+		authHeader   = ctx.GetHeader("Authorization")
+	)
+	if strings.Contains(strings.Join(cfg.RestrictedMethods, ","), method) {
+		authRequired = true
+	}
+
+	if !authRequired && len(cfg.RestrictedUrls) > 0 {
+		for _, e := range cfg.RestrictedUrls {
+			if strings.Contains(e, "*") && strings.Contains(url, strings.TrimSuffix(e, "/*")) {
+				authRequired = true
+				break
+			} else if strings.Contains(e, "*") && e == string(url[:strings.LastIndex(url, "/")]) {
+				authRequired = true
+				break
+			}
+		}
+	}
+
+	if authRequired {
+		for _, u := range cfg.Users {
+			if authHeader == "" {
+				ctx.Header("WWW-Authenticate", "Basic realm=Authorization Required")
+				ctx.AbortWithStatus(http.StatusUnauthorized)
+				return
+			}
+
+			credentials := strings.SplitN(authHeader, " ", 2)
+			if len(credentials) != 2 {
+				ctx.Header("WWW-Authenticate", "Basic realm=Authorization Required")
+				ctx.AbortWithStatus(http.StatusUnauthorized)
+				return
+			}
+
+			decodedCredentials, err := base64.StdEncoding.DecodeString(credentials[1])
+			if err != nil {
+				ctx.Header("WWW-Authenticate", "Basic realm=Authorization Required")
+				ctx.AbortWithStatus(http.StatusUnauthorized)
+				return
+			}
+
+			credentialsPair := strings.SplitN(string(decodedCredentials), ":", 2)
+			if len(credentialsPair) != 2 {
+				ctx.Header("WWW-Authenticate", "Basic realm=Authorization Required")
+				ctx.AbortWithStatus(http.StatusUnauthorized)
+				return
+			}
+
+			if credentialsPair[0] != u.UserName || credentialsPair[1] != u.Password {
+				ctx.Header("WWW-Authenticate", "Basic realm=Authorization Required")
+				ctx.AbortWithStatus(http.StatusUnauthorized)
+				return
+			}
+		}
+	}
+	ctx.Next()
+}
